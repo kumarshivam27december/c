906211c25a244377e7cda9b011993e404da7fbe6
Shivam kumar <kumarshivam16062004@gmail.com>
2024-03-02 20:38:52 +0530

    add two number linked list leetcode

---
+++ z_hards/add_two_number.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* temp = dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
+        }
+        return dummy->next;
+    }
+};
+
+int main() {
+    // Create linked lists
+    ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+    // Add the linked lists
+    ListNode* result = Solution().addTwoNumbers(l1, l2);
+
+    // Print the result
+    while (result) {
+        cout << result->val << " ";
+        result = result->next;
+    }
+    cout << endl;
+
+    return 0;
+}
