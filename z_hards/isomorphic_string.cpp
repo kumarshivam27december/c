846718e12aefb7e101667427e2816d37f494f630
Shivam kumar <kumarshivam16062004@gmail.com>
2024-03-04 23:17:53 +0530

    Isomorphic string leetcode alter solution

--- z_hards/isomorphic_string.cpp
+++ z_hards/isomorphic_string.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
 bool isisomorphic(string str1,string str2){
     int n = str1.length();
     unordered_map<char,char> mp1;
@@ -16,6 +17,24 @@ bool isisomorphic(string str1,string str2){
             mp1[str1[i]] = str2[i];
             flag[str2[i]] = true;
         }
+    }
+    return true;
+}
+*/
+bool isisomorphic(string str1,string str2){
+    unordered_map<char,char> mp1;
+    unordered_map<char,char> mp2;
+    int i = 0;
+    int j = 0;
+    while(i<str1.size() && j<str2.size()){
+        if(!mp1.count(str1[i]) and !mp2.count(str2[i])){
+            mp1[str1[i]]=str2[j];
+            mp2[str2[j]]=str1[i];
+        }else{
+            if(mp1[str1[i]]!=str2[j] or mp2[str2[j]]!=str1[i]) return false;
+        }
+        i++;
+        j++;
     }
     return true;
 }
