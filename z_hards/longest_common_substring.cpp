b7d5c147e15873ca7e7fbeae402a32d963f207e4
Shivam kumar <kumarshivam16062004@gmail.com>
2024-03-06 22:20:59 +0530

    updating questions

--- z_hards/longest_common_substring.cpp
+++ z_hards/longest_common_substring.cpp
@@ -9,19 +9,17 @@ public:
         int count = 0;
         int current = 1;
         int longest = 0;
-        for (int i = 1; i < n; i++) {
+        for(int i = 1;i<n;i++){
             if(nums[i]!=nums[i-1]){
-            if (nums[i] == nums[i - 1] + 1) {
-                current++; 
-            } else {
-                longest = max(longest, current); 
-                current = 1; 
-            }
+                if(nums[i]==nums[i-1]+1){
+                    current++;
+                }else{
+                    longest = max(longest,current);
+                    current = 1;
+                }
             }
         }
-
-        
-        return max(longest,current);
+        return longest;
     }
 };
 int main() {
