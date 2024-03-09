e0c0206b62ae72c4f8c103450aa71e2b363a0fee
Shivam kumar <kumarshivam16062004@gmail.com>
2024-03-10 01:13:28 +0530

    longest substring with no repeating character leetcode alter solution

--- z_hards/longest_substring_with_no_repeating_character.cpp
+++ z_hards/longest_substring_with_no_repeating_character.cpp
@@ -17,8 +17,30 @@ int lengthOfLongestSubstring(string s)
     }
     return maxlength;
 }
-int main(){
+
+/*
+    int lengthOfLongestSubstring(string s)
+    {
+        int n = s.size();
+        int ans = 0;
+        int char_count[128] = {0};
+        for (int l = 0, r = 0; r < n; r++)
+        {
+            char_count[s[r]]++;
+            while (char_count[s[r]] > 1)
+            {
+                char_count[s[l]]--;
+                l++;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+*/
+
+int main()
+{
     string s = "abcabcaabb";
-    cout<<lengthOfLongestSubstring(s)<<"\n";
+    cout << lengthOfLongestSubstring(s) << "\n";
     return 0;
 }
