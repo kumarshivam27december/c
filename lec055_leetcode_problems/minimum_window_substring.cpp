e3211d8cad7335e796cc2f26d08b2d04891dafa1
Shivam kumar <kumarshivam16062004@gmail.com>
2024-03-26 03:06:54 +0530

    minimum window substring

--- lec055_leetcode_problems/minimum_window_substring.cpp
+++ lec055_leetcode_problems/minimum_window_substring.cpp
@@ -1,27 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-string minWindow(string a,string b){
-    if(b.size()>a.size() || b.size()==0 ||a.size()==0){
-        return "";
-    }
-    vector<int> map(128,0);
-    int count = 0;
-    int left  = 0;
-    int right = 0;
-    int minlen = INT_MAX;
-    int startindex = 0;
-
-
-    for(char c:b){
-        map[c]++;
-    }
-
-    while(right < a.size()){
-        if(map[a[right++]]-- > 0){
-            count--;
+string minWindow(string s,string t){
+      if (s.empty() || t.empty() || s.length() < t.length()) {
+            return "";
         }
+
+        vector<int> map(128, 0);
+        int count = t.length();
+        int left = 0, right = 0, minLen = INT_MAX, startIndex = 0;
         
-    }
+        for (char c : t) {    //ADOBECODEBANC
+            map[c]++;          //ABC
+        }
+
+        while (right < s.length()) {
+            if (map[s[right++]]-- > 0) {
+                count--;
+            }
+
+            while (count == 0) {
+                if (right - left < minLen) {
+                    startIndex = left;
+                    minLen = right - left;
+                }
+
+                if (map[s[left++]]++ == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return minLen == INT_MAX ? "" : s.substr(startIndex, minLen);
 }
 int main(){
     string a = "ADOBECODEBANC";
