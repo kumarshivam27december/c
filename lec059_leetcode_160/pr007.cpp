752ef103d5311708edb8d3c0c3a2a003f6a022e0
Shivam kumar <kumarshivam16062004@gmail.com>
2024-04-03 02:03:04 +0530

    Best time to buy or sell stock

--- lec059_leetcode_160/pr007.cpp
+++ lec059_leetcode_160/pr007.cpp
@@ -22,3 +22,26 @@ Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+int result(vector<int> &v){
+    int profit = 0;
+    int buy_price = v[0];
+    for(int i =0;i<v.size();i++){
+        if(v[i]<buy_price){
+            buy_price = v[i];
+        }else{
+            int current_profit = v[i]-buy_price;
+            profit = max(profit,current_profit);
+        }
+    }
+    return profit;
+}
+int main(){
+
+    vector<int> v = {7,1,5,3,6,4};
+    int ans = result(v);
+    cout<<ans<<endl;
+    return 0;
+}
