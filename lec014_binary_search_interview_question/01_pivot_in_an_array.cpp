0a40b48481ebd2e5040694fbea9a2bdd5840b7d9
Shivam kumar <kumarshivam16062004@gmail.com>
2024-06-20 22:47:30 +0530

    tricky cp questions

--- lec014_binary_search_interview_question/01_pivot_in_an_array.cpp
+++ lec014_binary_search_interview_question/01_pivot_in_an_array.cpp
@@ -4,25 +4,21 @@ int pivot(int arr[],int n)
 {
     int s = 0;
     int e = n-1;
-    int mid = s + (e-s)/2;
     while(s<e)
     {
-        if(arr[mid]>=arr[0])
-        {
-            s = mid +1;
-        }
-        else
-        {
+        int mid = s+(e-s)/2;
+        if(arr[mid]>arr[e]){
+            s = mid+1;
+        }else{
             e = mid;
         }
-
-        mid = s + (e-s)/2;
+       
     }
     return s;
 }
 int main()
 {
-    int arr[7]={7,8,9,10,1,2,3};
+    int arr[7]={4, 5, 6 ,7 ,0 ,1, 2 };
     int ans = pivot(arr,7);
     cout<<ans<<endl;
     
