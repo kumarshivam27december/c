e2d16f60d347cb58aaa18c02b84cdc3da13b29a3
Shivam kumar <kumarshivam16062004@gmail.com>
2024-07-05 11:04:35 +0530

    number theory

--- lec065_number_theory/double_element.cpp
+++ lec065_number_theory/double_element.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h>
 #include <time.h>
 using namespace std;
+int finddublicate(vector<int>&ans){
+    // find all sum
+    // find the sum of the array
+    // find the sum of n natural number decrease in it
+    int sum = 0;
+    for(auto i:ans){
+        sum+=i;
+    }
+    int sum_of_natural_number = 0;
+    for(int i = 0;i<ans.size();i++){
+        sum_of_natural_number+=i;
+    }
 
-int main()
-{
-    clock_t tStart = clock();
-    vector<int>ans={1,2,3,4,3};
+    return sum-sum_of_natural_number;
+}
+int find_dublicate_using_xor(vector<int>&ans){
     int bns = ans[0];
     for(int i = 1;i<ans.size();i++){
         bns^=ans[i];
@@ -15,14 +26,15 @@ int main()
     for(int i = 0;i<ans.size();i++){
         bns2^=i;
     }
-
-
-    // find the sum of the array
-    // find the sum of n natural number decrease in it
-
     int ans2 = (bns^bns2);
-    cout<<ans2<<endl;
-    cout<<endl;
+    return ans2;
+}
+int main()
+{
+    clock_t tStart = clock();
+    vector<int>ans={1,2,3,4,3};
+    cout<<finddublicate(ans)<<endl;
+    cout<<find_dublicate_using_xor(ans)<<endl;
     printf("Time taken: %.2fms", (double)(clock() - tStart)/(CLOCKS_PER_SEC/1000000));
     cout<<endl;
     return 0;
