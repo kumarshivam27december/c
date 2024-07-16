aacc8db7c653d26d2a410c1445f5980a6576c0b3
Shivam kumar <kumarshivam16062004@gmail.com>
2024-07-16 21:47:41 +0530

    dp

--- lec067_dp/lcs_array_dp.cpp
+++ lec067_dp/lcs_array_dp.cpp
@@ -1,62 +1,62 @@
-#include <bits/stdc++.h>
-using namespace std;
-int dp[101][101];
-int recursion(int i,int j,vector<int> &a,vector<int> &b){
-    //base case
+// #include <bits/stdc++.h>
+// using namespace std;
+// int dp[101][101];
+// int recursion(int i,int j,vector<int> &a,vector<int> &b){
+//     //base case
     
-    if(i==a.size() ||j==b.size()){
-        return 0;
-    }
+//     if(i==a.size() ||j==b.size()){
+//         return 0;
+//     }
 
-    if(a[i]==b[j]){
-        return recursion(i+1,j+1,a,b)+1;
-    }else{
-        return max(recursion(i+1,j,a,b),recursion(i,j+1,a,b));
-    }
+//     if(a[i]==b[j]){
+//         return recursion(i+1,j+1,a,b)+1;
+//     }else{
+//         return max(recursion(i+1,j,a,b),recursion(i,j+1,a,b));
+//     }
 
-}
-int find_length_of_lcs(vector<int> &a,vector<int> &b){
-    return recursion(0,0,a,b);
-}
+// }
+// int find_length_of_lcs(vector<int> &a,vector<int> &b){
+//     return recursion(0,0,a,b);
+// }
 
-int answers(){
-    i = 0;
-    j = 0;
-    while(i<n && j<n){
-        if(s1[i]==s2[j]){
-            ans.push_back(s1[i]);
-            i++;
-            j++;
-        }else{
-            if(dp[i+1][j]>dp[i][j+1]){
-                i++;
-            }else{
-                j++;
-            }
-        }
-    }
-    return 
-}
+// int answers(){
+//     i = 0;
+//     j = 0;
+//     while(i<n && j<n){
+//         if(s1[i]==s2[j]){
+//             ans.push_back(s1[i]);
+//             i++;
+//             j++;
+//         }else{
+//             if(dp[i+1][j]>dp[i][j+1]){
+//                 i++;
+//             }else{
+//                 j++;
+//             }
+//         }
+//     }
+//     return 
+// }
 
-int main()
-{
-    for(int i = 0;i<101;i++){
-        for(int j = 0;j<101;j++){
-            dp[i][j]=-1;
-        }
-    }
-    vector<int>a = {1,2,3,4,1};
-    vector<int>b = {3,4,1,2,1,3};
-
-
-//    cout<<recursion(0,0,a,b)<<endl;
-    for(int i = 0;i<101;i++){
-        for(int j = 0;j<101;j++){
-            dp[i][j]=-1;
-        }
-    }
+// int main()
+// {
+//     for(int i = 0;i<101;i++){
+//         for(int j = 0;j<101;j++){
+//             dp[i][j]=-1;
+//         }
+//     }
+//     vector<int>a = {1,2,3,4,1};
+//     vector<int>b = {3,4,1,2,1,3};
 
 
+// //    cout<<recursion(0,0,a,b)<<endl;
+//     for(int i = 0;i<101;i++){
+//         for(int j = 0;j<101;j++){
+//             dp[i][j]=-1;
+//         }
+//     }
 
-    return 0;
-}
+
+
+//     return 0;
+// }
---
+++ lec067_dp/longest_common_substring.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string a = "abghc";
+    string b = "bghd";
+    // longest common substring
+
+    
+}
