b357329d24fdbdc54d05d423a42675955b0f16e8
Shivam kumar <kumarshivam16062004@gmail.com>
2024-08-02 00:02:54 +0530

    banking system

---
+++ lec069_projects/system.cpp
@@ -0,0 +1,30 @@
+/*
+
+add new person with min accound balance
+add to the person account
+remove from the person account
+show there transacation history
+and  that's it
+
+*/
+#include <iostream>
+using namespace std;
+int main(){
+	while(true){
+		cout<<"press 1 for adding user"<<endl;
+		cout<<"press 2 for showing bank balance"<<endl;
+		cout<<"press 3 for depositing money "<<endl;
+		cout<<"press 4 for  withdrawing the money"<<endl;
+		cout<<"press 5 for exiting"<<endl;
+		int n;
+		cin>>n;
+		if(n==1){
+			//adding user here
+		}else if(n==2){
+			//showing bank balance
+		}else if(n==3){
+			//depositing money in the account
+		}else if(n==4){
+	}
+	//cout<<"hello world";
+}
