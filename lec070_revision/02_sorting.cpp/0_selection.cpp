9d17cb4512da29f536bbd7f9ee453b7c90c92ec1
Shivam Kumar <kumarshivam16062004@gmail.com>
2025-01-01 21:17:01 +0530

    revision

--- lec070_revision/02_sorting.cpp/0_selection.cpp
+++ lec070_revision/02_sorting.cpp/0_selection.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,7 +13,7 @@ int main() {
     for(int i = 0;i<n;i++){
         int minindex = i;
         for(int j = i+1;j<n;j++){
-            if(arr[j]<arr[minindex]){
+            if(arr[j]< arr[minindex]){
                 minindex=j;
             }
         }        
@@ -22,6 +23,36 @@ int main() {
 
 
     for(auto i: arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void selection_sort(vector<int>&arr,int n,int idx){
+    if(idx==n-1){
+        return;
+    }
+
+    int curr_min = idx;
+    for(int i = idx+1;i<n;i++){
+        if(arr[i]<arr[curr_min]){
+            curr_min = i;
+        }
+    }
+    swap(arr[idx],arr[curr_min]);
+    selection_sort(arr,n,idx+1);
+}
+int main() {
+    int p;
+    cin>>p;
+    vector<int>arr(p);
+    for(int i = 0;i<p;i++){
+        cin>>arr[i];
+    }
+    selection_sort(arr,p,0);
+    for(auto i : arr){
         cout<<i<<" ";
     }
     return 0;
---
+++ lec070_revision/02_sorting.cpp/1_merge_sort.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+void merge(vector<int>&arr,int l,int r,int mid){
+    int l_size = mid-l+1;
+    int r_size = r-mid;
+    int left_arr[l_size+1];
+    int right_arr[l_size+1];
+    for(int i = 0;i<l_size;i++){
+        left_arr[i]=arr[i+l];
+    }
+    for(int i = 0;i<r_size;i++){
+        right_arr[i]= arr[i+mid+1];
+    }
+    left_arr[l_size] = right_arr[r_size] = INT_MAX;
+    int left_pointer = 0;
+    int right_pointer = 0;
+    for(int i = l;i<=r;i++){
+        if(left_arr[left_pointer] < right_arr[right_pointer]){
+            arr[i] = left_arr[left_pointer];
+            left_pointer++;
+        }else{
+            arr[i] = right_arr[right_pointer];
+            right_pointer++;
+        }
+    }
+}
+void mergesort(vector<int>&arr,int l,int r){
+    if(l==r){
+        return;
+    }
+    int mid = (l+r)/2;
+
+    mergesort(arr,l,mid);
+    mergesort(arr,mid+1,r);
+    merge(arr,l,r,mid);
+}
+int main() {
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    mergesort(arr,0,n-1);
+    for(auto i : arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
